c6b65f076d9840bebc81fc71cd76a2ae743c75d5
PETER <njugunahpeternjoroge@gmail.com>
2025-05-15 15:20:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const listE1=document.querySelector('.container')
+// fetch('./data.json')
+// .then(Res=>Res.json())
+// .then(data =>{
+//     data array.forEach(post => {
+        
+//     });
+// })
